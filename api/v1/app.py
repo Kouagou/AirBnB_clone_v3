da279e4ef0b208b9b275fa655e6b0242ba4bb294
Kouagou Kenneth <ncthakenneth@gmail.com>
2024-05-02 04:16:09 -0700

    Task 5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Starts a Flask web application
 """
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,6 +11,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns a JSON-formatted 404 status code response. """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
