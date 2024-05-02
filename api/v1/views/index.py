7b2f17960962c7af235e540df907af09fa525514
Kouagou Kenneth <ncthakenneth@gmail.com>
2024-05-02 03:23:58 -0700

    Task 3. Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Initialization
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def get_status():
+    """ Returns a JSON: "status": "OK" """
+    return jsonify({'status': 'OK'})
