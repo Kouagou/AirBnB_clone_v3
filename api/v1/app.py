f8e3746eab04148d1460f5e1c7e6f09aa05692db
Kouagou Kenneth <ncthakenneth@gmail.com>
2024-05-02 10:24:32 -0700

    Task 12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,14 @@ Starts a Flask web application
 """
 
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 
 
