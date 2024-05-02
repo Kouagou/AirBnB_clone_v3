bc7ed9e5656d053e2dec26fb20183f144d6a514e
Kouagou Kenneth <ncthakenneth@gmail.com>
2024-05-02 07:06:50 -0700

    6. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,8 +15,7 @@ def all():
     return jsonify(states)
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['GET'])
 def one(state_id):
     """ Retrieve a State object. """
     state = storage.get(State, state_id)
@@ -25,8 +24,7 @@ def one(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['DELETE'])
 def delete(state_id):
     """ Delete a State object. """
     state = storage.get(State, state_id)
@@ -50,8 +48,7 @@ def create():
     return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'],
-                 strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['PUT'])
 def update(state_id):
     """ Update a State. """
     if not request.get_json():
